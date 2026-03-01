bfe0d65142572be0c1042ed3ee84613920d0cd29
novexex <nnovexx@gmail.com>
2026-03-02 00:10:05 +0400

    Initial commit

---
+++ TradernetTestTask/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  TradernetTestTask
+//
+//  Created by Artur Ilyasov on 02.03.2026.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
